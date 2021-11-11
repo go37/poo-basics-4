55b6e378b9011ac7785e21561798adf41e3c62ea
Olivier Gourdonneau <olivier.gourdonneau@meteo.fr>
2021-11-11 11:27:50 +0100

    poo-4 second commit with corrections

--- Car.php
+++ Car.php
@@ -4,17 +4,40 @@ class Car
 {
     private bool $hasParkBrake = true;
 
-    public function setParkBrake(bool $hasParkBrake)
+    public function setParkBrake(bool $hasParkBrake): void
     {
         $this->hasParkBrake = $hasParkBrake;
     }
 
+    public function getParkBrake(): bool
+    {
+        return $this->hasParkBrake;
+    }
+
+    // exception with throw 
+
+    /* public function start(): string
+    {
+        if ($this->getParkBrake()) {
+            throw new Exception("Park brake is on");
+        } else {
+            return "Go !";
+        }
+    }*/
+
+    // exception with try / catch
+
     public function start(): string
     {
-        if ($this->setParkBrake() == false) {
-            return "Go !";
-        } else {
-            throw new Exception("Park brake is on");
+        try {
+            if ($this->getParkBrake()) {
+                throw new Exception("park brake is on.");
+            }
+        } catch (Exception $e) {
+            echo "Exception received : " . $e->getMessage() . "<br>" . PHP_EOL;
+            $this->setParkBrake(false);
+        } finally {
+            return  "My car rolls like a donut.";
         }
     }
 }
--- index.php
+++ index.php
@@ -4,6 +4,4 @@ require_once "Car.php";
 
 $car = new Car();
 
-$car->start();
-
-var_dump($car);
+echo $car->start();
